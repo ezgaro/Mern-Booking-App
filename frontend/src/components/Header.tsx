28a0081c5aaf953e94de4934f580d64e5b4b5ff5
ezgaro <ezgaro2005@gmail.com>
2024-12-22 20:53:12 +0200

    Implementing the Layout

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -5,12 +5,19 @@ const Header = () => {
     <div className="bg-blue-800 py-6">
       <div className="container mx-auto flex justify-between">
         <span className="text-3xl text-white font-bold tracking-tight">
-          <Link to='/'>MernHolidays.com</Link>
+          <Link to="/">MernHolidays.com</Link>
         </span>
         <span className="flex space-x-2">
-          <Link className="" to='/sign-in'></Link>
+          <Link
+            className="flex bg-white items-center text-blue-600 px-3 font-bold hover:bg-gray-100"
+            to="/sign-in"
+          >
+            Sign in
+          </Link>
         </span>
       </div>
     </div>
   );
 };
+
+export default Header;
