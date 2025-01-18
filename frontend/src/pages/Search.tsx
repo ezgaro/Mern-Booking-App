8eba6802033fc65e1639ffae5b617cf808854810
ezgaro <ezgaro2005@gmail.com>
2025-01-18 13:53:17 +0200

    Implementing the hotel cards

--- frontend/src/pages/Search.tsx
+++ frontend/src/pages/Search.tsx
@@ -2,6 +2,7 @@ import { useQuery } from "react-query";
 import { useSearchContext } from "../contexts/SearchContext";
 import * as apiClient from "../api-client.ts";
 import { useState } from "react";
+import SearchResultCard from "../components/SearchResultCard.tsx";
 
 const Search = () => {
   const search = useSearchContext();
@@ -20,7 +21,28 @@ const Search = () => {
     apiClient.searchHotels(searchParams)
   );
 
-  return <>Search Page</>;
+  return (
+    <div className="grid grid-cols-1 lg:grid-cols-[250px_1fr] gap-5">
+      <div className="rounded-lg border border-slate-300 p-5 sticky top-10">
+        <div className="space-y-5">
+          <h3 className="text-lg font-semibold border-b border-slate-300 pb-5">
+            Filter by:
+          </h3>
+        </div>
+      </div>
+      <div className="flex flex-col gap-5">
+        <div className="flex justify-between items-center">
+          <span className="text-xl font-bold">
+            {hotelData?.pagination.total} Hotels found
+            {search.destination ? ` in ${search.destination}` : ""}
+          </span>
+        </div>
+        {hotelData?.data.map((hotel) => (
+          <SearchResultCard hotel={hotel} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Search;
